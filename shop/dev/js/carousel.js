f3cfc0923e9894e9d961bbcdb6578b0e13a5b72e
raduiln <117358174+raduiln@users.noreply.github.com>
2023-03-01 16:39:45 +0200

    curs1

---
+++ shop/dev/js/carousel.js
@@ -0,0 +1,9 @@
+var myCarousel = document.querySelector("#myCarousel");
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: true,
+  pause: false, //'hover'
+  keyboard: true,
+  touch: true,
+  ride: false,
+});
