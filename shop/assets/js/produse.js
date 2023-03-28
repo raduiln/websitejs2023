d018fed562d79a81f287487d1b32b8ee93ea5634
raduiln <117358174+raduiln@users.noreply.github.com>
2023-03-28 13:09:36 +0300

    shop update

--- shop/assets/js/produse.js
+++ shop/assets/js/produse.js
@@ -2,68 +2,71 @@
 // https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline
 
 // Pas 4
-document.addEventListener('DOMContentLoaded', onLoad);
-console.log('apelez api')
+document.addEventListener("DOMContentLoaded", onLoad);
+console.log("apelez api");
 
 // Pas 1
 async function getProducts() {
-    let response = await fetch("https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline");
-    let products = await response.json();
-    return products;
+  let response = await fetch(
+    "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+  );
+
+  let products = await response.json();
+  return products;
 }
 
 // Pas 2
 function onLoad() {
-    document.getElementById('loader').classList.remove('hidden');
-    getProducts().then(function (products) {
-        console.log('incepe ap1')
-        listProducts(products).then(function (){
-           
-            var $container = $('.portfolioContainer');
-            var $filter = $('#filter');
-            $container.isotope({
-                filter: '.mascara',
-                layoutMode: 'masonry',
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear'
-                },
-                containerStyle: {
-                    position: 'relative',
-                    overflow: 'visible'
-                },
-            });
-            $filter.find('a').click(function() {
-                var selector = $(this).attr('data-filter');
-                $filter.find('a').removeClass('active');
-                $(this).addClass('active');
-                $container.isotope({
-                    filter: selector,
-                    animationOptions: {
-                        animationDuration: 750,
-                        easing: 'linear',
-                        queue: false,
-                    }
-                });
-                return false;
-            });
+  document.getElementById("loader").classList.remove("hidden");
+  getProducts()
+    .then(function (products) {
+      console.log("incepe ap1");
+      listProducts(products).then(function () {
+        var $container = $(".portfolioContainer");
+        var $filter = $("#filter");
+        $container.isotope({
+          filter: ".mascara",
+          layoutMode: "masonry",
+          animationOptions: {
+            duration: 750,
+            easing: "linear",
+          },
+          containerStyle: {
+            position: "relative",
+            overflow: "visible",
+          },
         });
-
-    }).catch(function (rej) {
-        console.log('Produsele nu au fost primite: ', rej);
-
-    }).finally(() => {
-        console.log("Cererea s-a incheiat: cu succes sau fara succes.");
-
+        $filter.find("a").click(function () {
+          var selector = $(this).attr("data-filter");
+          $filter.find("a").removeClass("active");
+          $(this).addClass("active");
+          $container.isotope({
+            filter: selector,
+            animationOptions: {
+              animationDuration: 750,
+              easing: "linear",
+              queue: false,
+            },
+          });
+          return false;
+        });
+      });
+    })
+    .catch(function (rej) {
+      console.log("Produsele nu au fost primite: ", rej);
+    })
+    .finally(() => {
+      console.log("Cererea s-a incheiat: cu succes sau fara succes.");
+      $.getScript("./assets/js/cart.js");
     });
 }
 
 // Pas 3
 async function listProducts(products) {
-    let html='';
-    products.forEach(function (product) {
-        //console.log(product);
-        let divProduct=`
+  let html = "";
+  products.forEach(function (product) {
+    //console.log(product);
+    let divProduct = `
         <div class="col-md-4 col-sm-6 mb-4 ${product.product_type}">
         <div class="card mb-30"><a class="card-img-tiles" href="#" data-abc="true">
                 <div class="inner">
@@ -76,16 +79,30 @@ async function listProducts(products) {
             <div class="card-body text-center">
                 <h4 class="card-title">${product.name} ${product.category}</h4>
                 <p class="text-muted">Starting from ${product.price}</p>
+                
                 <a target='_blank' class="btn btn-outline-primary btn-sm" href="${product.product_link}" data-abc="true">View Products</a>
+                <a href="#" data-id="${product.id}" data-name="${product.name}" data-price="${product.price}"
+                class="add-to-cart btn btn-primary">Add to Cart</a>
             </div>
         </div>
     </div>
        `;
-    html +=divProduct;
-    });
-    let container = document.getElementById('portfolioContainer');
-    container.innerHTML = html;
-    document.getElementById('loader').classList.add('hidden');
-
+    html += divProduct;
+  });
+  let container = document.getElementById("portfolioContainer");
+  container.innerHTML = html;
+  document.getElementById("loader").classList.add("hidden");
 }
-console.log('se termina api')
+console.log("se termina api");
+
+// dark mode
+
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "assets/img/sun.png";
+  } else {
+    icon.src = "assets/img/moon.png";
+  }
+};
